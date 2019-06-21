4827f7015362879cf72601a30550c74cc8cba21d
Apple <apple@Apples-MacBook-Pro.local>
2019-06-21 10:27:19 -0600

    final-project

---
+++ index.js
@@ -0,0 +1,30 @@
+var typeE = $('.typeE')
+	var typeI = $('.typeI')
+	var typeS = $('.typeS')
+	var typeN = $('.typeN')
+	var typeT = $('.typeT')
+	var typeF = $('.typeF')
+	var typeJ = $('.typeJ')
+	var typeP = $('.typeP')
+	var results = $('.button')
+
+	results.on("click", console.log("IT WORKED"));
+	function appendAnswers(){
+	  console.log("IT WORKED");
+	  if (typeI.checked == true && typeS.checked == true && typeT.checked == true && typeJ.checked == true) {
+	console.log("IT WORKED");
+  if else (typeI.checed == true && typeS.checked == true && typeT.checked == true && typeP.checked == true){
+
+  }if else (typeI.checked == true && typeS.checked == true && typeF.checked == true && typeJ.checked == true){
+
+  }if else (typeI.checked == true && typeS.checked == true && typeF.checked == true && typeP.checked == true){
+
+  }if else (typeI.checked == true && typeN.checked == true && typeT.checked == true && typeP.checked == true){
+
+  }if else (typeI.checked == true && typeN.checked == true && typeT.checked == true && typeJ.checked == true){
+
+  }if else (typeI.checked == true && typeN.checked == true && typeF.cheked == true && typeJ.checked == true){
+
+  }if else (typeI.checked == true && typeN.checked == true && typeF.checked == true && typeP.checked == true)
+	}
+localStorage.setItem("key1","");
